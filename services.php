46df81a8453d8c60b3b03acfe4b8f312eb8b4aeb
chrisalex-ochay <69009810+chrisalex-ochay@users.noreply.github.com>
2021-08-03 12:05:33 +0300

    Add files via upload

---
+++ services.php
@@ -0,0 +1,333 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>At your Service - Freedom World Technolories & Accesories</title>
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Link for awesome fonts cdn -->
+    <link href="https://fonts.googleapis.com/css2?
+    family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"> 
+
+    <!-- link for icon font awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" 
+    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="shortcut icon" href="images/logo-dell.png" type="image/x-icon">
+
+    <!-- Link for starts ratings from fonts cdn -->
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/
+    fontawesome.min.css"> -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <!-- owl carousel css file cdn link -->
+    <link rel="stylesheet" href="https:cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+
+    <!-- fontawesome cdn -->
+    <link rel="stylesheet" type="text/css" href="https:cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+
+    <!-- Bootstrap and J-query library links -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<!-- ------------------------- php header link --------------------------  -->
+    <?php include('header2.php'); ?>
+
+
+<!-- --------------------------- best deals services start-----------------  -->
+
+<section class="service1">                                   
+    <div class="box-container">
+
+                                <!-- WEBSITE DEVELOPEMENT -->
+        <div class="offer" id="webdev">
+            <div class="small-container">
+            <h1 class="heading2"><span>Website Development</span></h1>
+                <div class="row">
+                    <div class="col-2">
+                        <img src="images/deal1-web-dev1.png" class="offer-img">
+                    </div>
+                    <div class="col-2">
+                    <!-- <h3>Website Development</h3> -->
+                        <p>We do</p>
+                        <div class="service-details">
+                            <ol>
+                                <li>Website Hosting</li>
+                                <li>Domain Registration</li>
+                                <li>Website Designing <br><span>For all kinds of Websites like</span>
+                                    <ul>
+                                        <li>Personal Website</li>
+                                        <li>Company Website</li>
+                                        <li>E-commerce Website</li>
+                                        <li>NGO Website</li>
+                                        <li>And More</li>
+                                    </ul>
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+                                <!-- MOBILE APP DEVELOPEMENT -->
+        <div class="offer" id="mobappdev">
+            <div class="small-container">
+                <h1 class="heading2"><span>Mobile App Development</span></h1>
+                <div class="row">
+                    <div class="col-2">
+                        <img src="images/deal2-mobile-app-devt.png" class="offer-img">
+                    </div>
+                    <div class="col-2">
+                        <p>We develop Mobile Applications for</p>
+                        <div class="service-details">
+                            <ol>
+                                <li>Android Platform</li>
+                                <li>OS-platforms</li>
+                                <li>Windows Phones platform coming soon.</li>
+                            </ol>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
+                                        <!-- SOFTWARE/SYSTEMS DEVELOPEMENT -->
+        <div class="offer" id="softwaredev">
+            <div class="small-container">
+            <h1 class="heading2"><span>Software/Systems Development</span></h1>
+                <div class="row">
+                    <div class="col-2">
+                        <img src="images/deal3-tech-web.png" class="offer-img">
+                    </div>
+                    <div class="col-2">
+                        <p>We delevope all kinds of Softwares and Systems</p>
+                        <div class="service-details">
+                            <ol>
+                                <li>Entrepreneur Resource Program(ERP)</li>
+                                <li>Sacco Management System</li>
+                                <li>Point Of Sale systems(POS)</li>
+                                <li>Education Learning Portals</li>
+                                <li>Records Management Software</li>
+                                <li>Any Many More</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+                                <!-- SOFTWARE INSTALLATIONS -->
+        <div class="offer" id="softwareInstall">
+            <div class="small-container">
+            <h1 class="heading2"><span>Software Installations</span></h1>
+                <div class="row">
+                    <div class="col-2">
+                        <img src="images/deal4-install-softwares.png" class="offer-img">
+                    </div>
+                    <div class="col-2">
+                        <p>We install all types of softwares</p>
+                        <div class="service-details">
+                            <ol>
+                                <li>Asycuda World Installations</li>
+                                <li>MicroSoft Windows Operating System
+                                    <ul>
+                                        <li>Windows 10</li>
+                                        <li>Windows 8.1</li>
+                                        <li>Windows 8</li>
+                                        <li>Windows 7</li>
+                                        <li>Windows XP</li>
+                                    </ul>
+                                </li>
+                                <li>Linux
+                                    <ul>
+                                        <li>UBUNTU 20</li>
+                                        <li>Kali Linux</li>
+                                        <li>Red Hat Linux</li>
+                                        <li>Debian</li>
+                                        <li>And More</li>
+                                    </ul>
+                                </li>
+                                <li>MicroSoft Office
+                                    <ul>
+                                        <li>Office 2019</li>
+                                        <li>Office 2016</li>
+                                        <li>Office 2013</li>
+                                        <li>Office 2010</li>
+                                        <li>Office 2007</li>
+                                    </ul>
+                                </li>
+                                <li>Anti-Viruses
+                                    <ul>
+                                        <li>Latest Kaspersky</li>
+                                        <li>Latest Avast</li>
+                                        <li>Latest Smadav</li>
+                                        <li>And Others</li>
+                                    </ul>
+                                </li>
+                                <li>And Many More Installations</lv>
+                            </ol>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+                                       <!-- ISP SERVICES -->
+        <div class="offer" id="ispServices">
+            <div class="small-container">
+            <h1 class="heading2"><span>Internet Service Provider(ISP) services</span></h1>
+                <div class="row">
+                    <div class="col-2">
+                        <img src="images/deal5-ISP-Broadband.png" class="offer-img">
+                    </div>
+                    <div class="col-2">
+                        <p>We delevope all kinds of Softwares and Systems</p>
+                        <div class="service-details">
+                            <ol>
+                                <li>Fibre Cable Internet like Zuku, Simba Telecom</li>
+                                <li>Mobile Internet like for Smart Mobile, Airtel, etc</li>
+                                <li>Liquid Telecom Internet</li>
+                                <li>Any Many More</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+
+                                              <!-- AUTOMATIC TIMETABLE SERVICES -->
+        <div class="offer" id="timetableAuto">
+            <div class="small-container">
+            <h1 class="heading2"><span>Timetable Automation</span></h1>
+                <div class="row">
+                    <div class="col-2">
+                        <img src="images/deal6-GBN-Timetable-.png" class="offer-img">
+                    </div>
+                    <div class="col-2">
+                        <p>we deal in automation of tabletables for all institutions like</p>
+                        <div class="service-details">
+                            <ol>
+                                <li>Universities</li>
+                                <li>Secondary Schools</li>
+                                <li>Primary Schools</li>
+                                <li>Any Many More</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+                                              <!-- COMPUTER LITERACY LESSONS -->
+        <div class="offer" id="complessons">
+            <div class="small-container">
+            <h1 class="heading2"><span>Computer Literacy Lessons</span></h1>
+                <div class="row">
+                    <div class="col-2">
+                        <img src="images/deal7-work1.png" class="offer-img">
+                    </div>
+                    <div class="col-2">
+                        <p>Computer fundamentals for all categories from children to adults like</p>
+                        <div class="service-details">
+                            <ol>
+                                <li>MicroSoft Word,Excel,PowerPoint</li>
+                                <li>Introduction to Computer fundamentals</li>
+                                <li>Techniques on how to navigate Internet</li>
+                                <li>Introduction to Databases</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+                                              <!-- INTERNET CAFE SERVICES -->
+        <div class="offer" id="internetCafe">
+            <div class="small-container">
+            <h1 class="heading2"><span>Internet Cafe Services</span></h1>
+                <div class="row">
+                    <div class="col-2">
+                        <img src="images/deal8-cafe-computers.png" class="offer-img">
+                    </div>
+                    <div class="col-2">
+                        <p>Need of high speed internet for live streaming, Video Calls, 
+                            Conferencing and many more,
+                            We've got you covered and our internet cafe</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+                                                    <!-- PHONES/COMPUTERS REPAIRING -->
+        <div class="offer" id="phoneRepaing">
+            <div class="small-container">
+            <h1 class="heading2"><span>Phone/Computer Repairing</span></h1>
+                <div class="row">
+                    <div class="col-2">
+                        <img src="images/deal9-computer-repairing1.png" class="offer-img">
+                    </div>
+                    <div class="col-2">
+                        <p>We handle all types of repairing of phones and computers </p>
+                        <div class="service-details">
+                            <ol>
+                                <li>Broken Screen Replacement</li>
+                                <li>Charging/Power System</li>
+                                <li>Laptop Hinges</li>
+                                <li>Battery Replacement</li>
+                                <li>And Many More</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+                                                    <!-- GENERAL ICT CONSULTANCY -->
+        <div class="offer" id="ictConsult">
+            <div class="small-container">
+            <h1 class="heading2"><span>General I.T consultancy</span></h1>
+                <div class="row">
+                    <div class="col-2">
+                        <img src="images/deal10-ICT-consultancy.png" class="offer-img">
+                    </div>
+                    <div class="col-2">
+                        <p>we advice you on how to go about the changing Technologies</p>
+                        <div class="service-details">
+                            <ol>
+                                <li>Softwares or Systems that suit your needs</li>
+                                <li>Whether to maintain or develop new system</li>
+                                <li>General ICT assessment</li>
+                                <li>Internet Related Issues</li>
+                                <li>And Many More</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- --------------------------- best deals services end-----------------  -->
+
+
+<!----------------------    footer php link  ---------------------->
+<?php include('footer2.php'); ?>
+
+<!---------------    jquery cdn link    --------------->
+<script src="https:cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!---------------    owl carousel js file cdn link    --------------->
+<script src="https:cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+<script src="main.js"></script>
+
+</body>
+</html>
